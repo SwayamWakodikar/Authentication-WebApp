c8b0a33acadb88be96ac4db416349234e6287b8c
SwayamWakodikar <swayam.w06@gmail.com>
2025-12-11 22:56:53 +0530

    Started Frontend Backend Done

--- server/config/passport.ts
+++ server/config/passport.ts
@@ -2,38 +2,34 @@ import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import User from "../model/user.js";
 
-passport.serializeUser((user: any,done)=>{
-    done(null,user.id);
+passport.serializeUser((user: any, done) => {
+    done(null, user.id);
 });
 
-passport.deserializeUser(async(id: string,done)=>{
-    try{
-        const user=await User.findById(id);
-        done(null,user);
+passport.deserializeUser(async (id: string, done) => {
+    try {
+        const user = await User.findById(id);
+        done(null, user);
     }
-    catch(error){
-        done(error,null);
+    catch (error) {
+        done(error, null);
     }
 });
 passport.use(new GoogleStrategy({
-    // Credentials from your .env file
     clientID: process.env.GOOGLE_CLIENT_ID!,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL!,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
     passReqToCallback: true,
 },
     async (request, accessToken, refreshToken, profile, done) => {
         try {
             const email = profile.emails?.[0]?.value;
             const googleId = profile.id;
-            
             let existingUser = await User.findOne({ googleId });
-            
             if (existingUser) {
                 return done(null, existingUser);
             }
             existingUser = await User.findOne({ email });
-            
             if (existingUser) {
                 existingUser.googleId = googleId;
                 await existingUser.save();
@@ -43,15 +39,15 @@ passport.use(new GoogleStrategy({
                 googleId: googleId,
                 name: profile.displayName,
                 email: email,
-                username: email?.split('@')[0] + Math.floor(Math.random() * 10000).toString().padStart(4, '0') 
+                username: email?.split('@')[0] + Math.floor(Math.random() * 10000).toString().padStart(4, '0')
             });
-
             await newUser.save();
             done(null, newUser);
 
-        } catch (err) {
-            console.error("Error during Google OAuth:", err);
-            done(err as Error, false);
+        }
+        catch (error) {
+            console.error("Error Login in ", error);
+            done(error as Error, false);
         }
     }
-));
+))
--- server/routes/authRoute.ts
+++ server/routes/authRoute.ts
@@ -1,9 +1,20 @@
 import {Request, Response, Router} from "express";
 import { handleUserSignUp } from "../controllers/authController.js";
+import passport from "passport";
 const router =Router();
 
-router.post('/signup',handleUserSignUp);
+router.post('/signup',handleUserSignUp);//for manaul email and password
+
+router.get('/get',passport.authenticate('google',{//to send user to google login page
+    scope:['profile','email']
+}));
+
+router.get('/google/callback',passport.authenticate('google',{
+    successRedirect: 'http://localhost:5173/dashboard',//dummy links until frontend is made
+    failureRedirect: 'http://localhost:5173/login'
+}));
+
 router.get('/',(req:Request,res:Response)=>{
     res.send("Authentication Route Works")
-})
+});
 export default router;
